1ad677c880740025cc345e6d86583e0e6c1630f3
markusremplbauer <markus.remplbauer@gmx.at>
2021-12-27 22:15:35 +0100

    update Person validation

--- backend/src/main/java/at/htl/vehicleworkshop/entity/Person.java
+++ backend/src/main/java/at/htl/vehicleworkshop/entity/Person.java
@@ -4,7 +4,10 @@ import at.htl.vehicleworkshop.control.validation.HasLegalAgeConstraint;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.Objects;
 
@@ -16,15 +19,21 @@ public class Person extends PanacheEntityBase {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "P_ID")
     public Long id;
+
     @NotBlank(message = "Name should not be blank")
     @Column(name = "P_NAME")
     public String name;
+
     @NotBlank(message = "Email should not be blank")
+    @Email
     @Column(name = "P_EMAIL")
     public String email;
+
     @Column(name = "P_PHONE_NUMBER")
     public String phoneNumber;
+
     @HasLegalAgeConstraint
+    @NotNull(message = "DOB should not be null")
     @Column(name = "P_DOB")
     public LocalDate dob;
 
